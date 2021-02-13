7cf9a90bf2cc579dc8119cc8a41a3b1d79325728
NazarovaVictoria <77588631+NazarovaVictoria@users.noreply.github.com>
2021-02-13 21:50:42 +0200

    Add files via upload

---
+++ js/dist/main.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$('.slider-image').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true
+});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.slider-image').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+   vertical:true,
+	verticalSwiping:true
+});
