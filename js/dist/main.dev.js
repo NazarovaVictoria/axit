b9f3973627028a543eddcb27b4a4b1d714ace4a7
NazarovaVictoria <77588631+NazarovaVictoria@users.noreply.github.com>
2021-02-27 19:15:33 +0200

    Add files via upload

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,8 +1,32 @@
-"use strict";
+'use strict';
 
-$('.slider-image').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  vertical: true,
-  verticalSwiping: true
-});
+function Tabs() {
+  var bindAll = function bindAll() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+
+    for (var i = 0; i < menuElements.length; i++) {
+      menuElements[i].addEventListener('click', change, false);
+    }
+  };
+
+  var clear = function clear() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+
+    for (var i = 0; i < menuElements.length; i++) {
+      menuElements[i].classList.remove('active');
+      var id = menuElements[i].getAttribute('data-tab');
+      document.getElementById(id).classList.remove('active');
+    }
+  };
+
+  var change = function change(e) {
+    clear();
+    e.target.classList.add('active');
+    var id = e.currentTarget.getAttribute('data-tab');
+    document.getElementById(id).classList.add('active');
+  };
+
+  bindAll();
+}
+
+var connectTabs = new Tabs();
